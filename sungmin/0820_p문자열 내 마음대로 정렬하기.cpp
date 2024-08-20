cef935412ef7c9f6c90447fd292bf28a27cc4629
yhc06141 <133004743+yhc06141@users.noreply.github.com>
2024-08-20 22:27:45 +0900

    이성민_0820_p문자열 내 마음대로 정렬하기

---
+++ sungmin/0820_p문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<string> solution(vector<string> strings, int n) {
+    vector<string> answer;
+    for(int i=0;i<strings.size()-1;i++)
+    {
+       for(int j=i+1;j<strings.size();j++) 
+       {
+           if(strings[i][n]>strings[j][n])
+           {
+               swap(strings[i],strings[j]);
+           }
+           else if(strings[i][n]==strings[j][n])
+           {
+               if(strings[i]>strings[j])
+               {
+                   swap(strings[i],strings[j]);
+               }
+           }
+       }
+    }
+    answer=strings;
+    return answer;
+}
